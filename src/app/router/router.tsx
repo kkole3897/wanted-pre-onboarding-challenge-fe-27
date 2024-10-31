8a97ae6ec99b6533d3c450d1692147a923e3a153
jinkwan <kkole3897@gmail.com>
2024-11-01 03:40:21 +0900

    feat: ProtectedRoute 추가

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import { HomePage } from '@/pages/home';
 import { auth } from './auth';
+import { ProtectedRoute } from '../ui';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
+    element: (
+      <ProtectedRoute>
+        <HomePage />
+      </ProtectedRoute>
+    ),
   },
   auth,
 ]);
