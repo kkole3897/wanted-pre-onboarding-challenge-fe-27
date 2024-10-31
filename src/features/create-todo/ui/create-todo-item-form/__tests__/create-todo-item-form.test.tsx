2558a3e769dfddfc706b62885f9048263bf0f5f8
jinkwan <kkole3897@gmail.com>
2024-11-01 06:43:28 +0900

    feat(CreateTodoItemForm): 처리 중인 상태 추가

--- src/features/create-todo/ui/create-todo-item-form/__tests__/create-todo-item-form.test.tsx
+++ src/features/create-todo/ui/create-todo-item-form/__tests__/create-todo-item-form.test.tsx
@@ -10,28 +10,28 @@ import CreateTodoItemForm, {
 function renderCreateTodoItemForm(props?: Partial<CreateTodoItemFormProps>) {
   const result = render(<CreateTodoItemForm {...props} />);
 
-  const TitleInput = result.getByPlaceholderText('제목') as HTMLInputElement;
-  const ContentInput = result.getByPlaceholderText(
-    '내용'
-  ) as HTMLTextAreaElement;
-  const SubmitButton = result.getByText('추가') as HTMLButtonElement;
+  const TitleInput = () => result.getByPlaceholderText('제목');
+  const ContentInput = () => result.getByPlaceholderText('내용');
+  const SubmitButton = () => result.getByText('추가');
+  const SubmittingButton = () => result.queryByText('처리 중...');
 
   const changeTitle = async (title: string) => {
-    await userEvent.type(TitleInput, title);
+    await userEvent.type(TitleInput(), title);
   };
 
   const changeContent = async (content: string) => {
-    await userEvent.type(ContentInput, content);
+    await userEvent.type(ContentInput(), content);
   };
 
   const submit = async () => {
-    await userEvent.click(SubmitButton);
+    await userEvent.click(SubmitButton());
   };
 
   return {
     TitleInput,
     ContentInput,
     SubmitButton,
+    SubmittingButton,
     changeTitle,
     changeContent,
     submit,
@@ -44,13 +44,14 @@ beforeEach(() => {
 
 describe('<CreateTodoItemForm />', () => {
   it('기본 엘리먼트 렌더링', () => {
-    const { TitleInput, ContentInput, SubmitButton } =
+    const { TitleInput, ContentInput, SubmitButton, SubmittingButton } =
       renderCreateTodoItemForm();
 
-    expect(TitleInput).toBeInTheDocument();
-    expect(ContentInput).toBeInTheDocument();
-    expect(SubmitButton).toBeInTheDocument();
-    expect(SubmitButton).toBeDisabled();
+    expect(TitleInput()).toBeInTheDocument();
+    expect(ContentInput()).toBeInTheDocument();
+    expect(SubmitButton()).toBeInTheDocument();
+    expect(SubmitButton()).toBeDisabled();
+    expect(SubmittingButton()).not.toBeInTheDocument();
   });
 
   it('제목과 내용을 입력할 수 있다.', async () => {
@@ -60,8 +61,8 @@ describe('<CreateTodoItemForm />', () => {
     await changeTitle('1');
     await changeContent('2');
 
-    expect(TitleInput).toHaveValue('1');
-    expect(ContentInput).toHaveValue('2');
+    expect(TitleInput()).toHaveValue('1');
+    expect(ContentInput()).toHaveValue('2');
   });
 
   it('제목을 입력하면 추가 버튼이 활성화된다.', async () => {
@@ -69,7 +70,7 @@ describe('<CreateTodoItemForm />', () => {
 
     await changeTitle('1');
 
-    expect(SubmitButton).toBeEnabled();
+    expect(SubmitButton()).toBeEnabled();
   });
 
   it('제출 버튼을 누르면 onSubmit 이벤트가 호출된다.', async () => {
@@ -86,5 +87,12 @@ describe('<CreateTodoItemForm />', () => {
       title: '1',
       content: '2',
     });
+  });
+
+  it('제출 중에는 추가 버튼이 처리 중으로 바뀌고, 비활성화 된다.', () => {
+    const { SubmittingButton } = renderCreateTodoItemForm({ submitting: true });
+
+    expect(SubmittingButton()).toBeInTheDocument();
+    expect(SubmittingButton()).toBeDisabled();
   });
 });
--- src/features/create-todo/ui/create-todo-item-form/create-todo-item-form.tsx
+++ src/features/create-todo/ui/create-todo-item-form/create-todo-item-form.tsx
@@ -2,10 +2,12 @@ import { useCreateTodoItemFormValues } from '../../lib';
 import { CreateTodoItemData } from '../../model';
 
 export type CreateTodoItemFormProps = {
+  submitting?: boolean;
   onSubmit?: (data: CreateTodoItemData) => void;
 };
 
 export default function CreateTodoItemForm({
+  submitting = false,
   onSubmit,
 }: CreateTodoItemFormProps) {
   const { register, isValid, createHandleSubmit } =
@@ -18,8 +20,8 @@ export default function CreateTodoItemForm({
       <div>
         <input {...register('title')} type="text" placeholder="제목" />
         <textarea name="content" placeholder="내용" />
-        <button type="submit" disabled={!isValid}>
-          추가
+        <button type="submit" disabled={!isValid || submitting}>
+          {submitting ? '처리 중...' : '추가'}
         </button>
       </div>
     </form>
