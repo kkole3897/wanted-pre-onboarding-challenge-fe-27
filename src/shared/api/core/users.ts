1b66db21d28e5950398ff6878aa6fb4ad52a6cca
jinkwan <kkole3897@gmail.com>
2024-11-01 04:39:16 +0900

    feat(api/core/users): loginByEmail 추가

--- src/shared/api/core/users.ts
+++ src/shared/api/core/users.ts
@@ -43,3 +43,41 @@ export const registerByEmail = async (
 
   return validateRegisterResponse(response.data);
 };
+
+type LoginByEmailPayload = {
+  email: string;
+  password: string;
+};
+
+const LoginResponseSchema = z.object({
+  message: z.string(),
+  token: z.string(),
+});
+
+type LoginResponse = z.infer<typeof LoginResponseSchema>;
+
+const validateLoginResponse = (data: unknown): LoginResponse => {
+  return LoginResponseSchema.parse(data);
+};
+
+const LoginErrorDataSchema = z.object({
+  message: z.string(),
+});
+
+type LoginErrorData = z.infer<typeof LoginErrorDataSchema>;
+
+type LoginError = AxiosError<LoginErrorData>;
+
+export const isLoginError = (error: unknown): error is LoginError => {
+  if (!axios.isAxiosError(error)) {
+    return false;
+  }
+
+  return LoginErrorDataSchema.safeParse(error.response?.data).success;
+};
+
+export const loginByEmail = async (payload: LoginByEmailPayload) => {
+  const response = await client.post('/users/login', payload);
+
+  return validateLoginResponse(response.data);
+};
