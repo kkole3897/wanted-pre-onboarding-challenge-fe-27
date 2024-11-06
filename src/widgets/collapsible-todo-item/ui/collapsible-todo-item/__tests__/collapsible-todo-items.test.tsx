7167a9f559362bf6804fb3541d23611c64deecc2
jinkwan <kkole3897@gmail.com>
2024-11-06 23:01:38 +0900

    feat(CollapsibleTodoItem): 열기/닫기 기능 추가

--- src/widgets/collapsible-todo-item/ui/collapsible-todo-item/__tests__/collapsible-todo-items.test.tsx
+++ src/widgets/collapsible-todo-item/ui/collapsible-todo-item/__tests__/collapsible-todo-items.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/vitest';
 import { describe, it, expect, beforeEach } from 'vitest';
 import { render, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import CollapsibleTodoItem, {
   type CollapsibleTodoItemProps,
@@ -13,12 +14,21 @@ function renderCollapsibleTodoItem(props: CollapsibleTodoItemProps) {
 
   const Content = () => result.queryByText(props.todo.content);
 
-  const Trigger = () => result.getByText('열기');
+  const OpenButton = () => result.queryByText('열기');
+
+  const CloseButton = () => result.queryByText('닫기');
+
+  const open = async () => userEvent.click(OpenButton()!);
+
+  const close = async () => userEvent.click(CloseButton()!);
 
   return {
     Title,
     Content,
-    Trigger,
+    OpenButton,
+    CloseButton,
+    open,
+    close,
   };
 }
 
@@ -28,7 +38,7 @@ beforeEach(() => {
 
 describe('<CollapsibleTodoItem />', () => {
   it('기본 상태는 접혀있는 상태이다.', () => {
-    const { Title, Content, Trigger } = renderCollapsibleTodoItem({
+    const { Title, Content, OpenButton } = renderCollapsibleTodoItem({
       todo: {
         id: '1',
         title: '제목',
@@ -40,6 +50,43 @@ describe('<CollapsibleTodoItem />', () => {
 
     expect(Title()).toBeInTheDocument();
     expect(Content()).not.toBeInTheDocument();
-    expect(Trigger()).toBeInTheDocument();
+    expect(OpenButton()).toBeInTheDocument();
+  });
+
+  it('열기 버튼을 누르면 내용이 나타난다.', async () => {
+    const { Content, open, CloseButton } = renderCollapsibleTodoItem({
+      todo: {
+        id: '1',
+        title: '제목',
+        content: '내용',
+        createdAt: '2024-10-31T21:56:05.364Z',
+        updatedAt: '2024-10-31T21:56:05.364Z',
+      },
+    });
+
+    await open();
+
+    expect(Content()).toBeInTheDocument();
+    expect(CloseButton()).toBeInTheDocument();
+  });
+
+  it('닫기 버튼을 누르면 내용이 사라진다.', async () => {
+    const { Content, open, close, CloseButton, OpenButton } =
+      renderCollapsibleTodoItem({
+        todo: {
+          id: '1',
+          title: '제목',
+          content: '내용',
+          createdAt: '2024-10-31T21:56:05.364Z',
+          updatedAt: '2024-10-31T21:56:05.364Z',
+        },
+      });
+
+    await open();
+    await close();
+
+    expect(Content()).not.toBeInTheDocument();
+    expect(CloseButton()).not.toBeInTheDocument();
+    expect(OpenButton()).toBeInTheDocument();
   });
 });
--- src/widgets/collapsible-todo-item/ui/collapsible-todo-item/collapsible-todo-item.tsx
+++ src/widgets/collapsible-todo-item/ui/collapsible-todo-item/collapsible-todo-item.tsx
@@ -1,4 +1,5 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
+import { useState } from 'react';
 
 import { type TodoItem } from '@/entities/todo';
 
@@ -9,12 +10,16 @@ export type CollapsibleTodoItemProps = {
 export default function CollapsibleTodoItem({
   todo,
 }: CollapsibleTodoItemProps) {
+  const [isOpened, setIsOpened] = useState(false);
+
   return (
-    <Collapsible.Root>
+    <Collapsible.Root open={isOpened} onOpenChange={setIsOpened}>
       <div>
         <div>{todo.title}</div>
         <div>
-          <Collapsible.Trigger>열기</Collapsible.Trigger>
+          <Collapsible.Trigger>
+            {isOpened ? '닫기' : '열기'}
+          </Collapsible.Trigger>
         </div>
       </div>
       <Collapsible.Content>{todo.content}</Collapsible.Content>
